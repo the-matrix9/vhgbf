e538736d4605b20da182050b46f5259504300f88
𝗧𝗵𝗲_𝗺𝗮𝘁𝗿𝗶𝘅𝟵 <anshppt19@gmail.com>
2025-12-01 06:33:26 +0530

    ─ ❖ | ʀɪsʜυ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖ ─

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+
+API_ID = int(getenv("API_ID", "14050586")) #optional
+API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c") #optional
+
+MONGO_DB_NAME = "TelegramBot"
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7432319742").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7432319742"))
+MONGO_URL = getenv("MONGO_URL", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/svssj2.jpg')
+ALIVE_TEXT = getenv("ALIVE_TEXT")
+PM_LOGGER = getenv("PM_LOGGER","-1002294781733")
+LOG_GROUP = getenv("LOG_GROUP","-1002294781733")
+GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/Botminetech/USERBOT-GARIB")
+BRANCH = getenv("BRANCH", "main") #don't change
+ 
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
